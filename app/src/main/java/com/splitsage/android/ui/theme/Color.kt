c48ddd4f5bb8898ff008887341b32b8c48e8c1ff
Latex <M.Hassan17877@student.aast.edu>
2025-03-09 21:32:17 +0200

    Add color definitions for theme

---
+++ app/src/main/java/com/splitsage/android/ui/theme/Color.kt
@@ -0,0 +1,66 @@
+package com.splitsage.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary brand colors
+val Primary = Color(0xFF2E7D32)
+val PrimaryDark = Color(0xFF005005)
+val PrimaryLight = Color(0xFF60AD5E)
+
+// Secondary colors
+val Secondary = Color(0xFF5E35B1) 
+val SecondaryDark = Color(0xFF280680)
+val SecondaryLight = Color(0xFF9162E4)
+
+// Accent colors
+val Accent = Color(0xFFFF6D00)
+val AccentDark = Color(0xFFC43C00)
+val AccentLight = Color(0xFFFF9E40)
+
+// Error colors
+val Error = Color(0xFFF44336)
+val ErrorDark = Color(0xFFB71C1C)
+val ErrorLight = Color(0xFFFFCDD2)
+
+// Warning colors
+val Warning = Color(0xFFFFA000)
+val WarningDark = Color(0xFFF57F17)
+val WarningLight = Color(0xFFFFECB3)
+
+// Text colors
+val TextPrimary = Color(0xFF212121)
+val TextSecondary = Color(0xFF757575)
+val TextTertiary = Color(0xFF9E9E9E)
+
+// Basic colors
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// Background colors
+val Background = Color(0xFFF5F5F5)
+val Surface = Color(0xFFFFFFFF)
+val DividerColor = Color(0xFFE0E0E0)
+
+// Dark theme colors
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val DividerDark = Color(0xFF303030)
+
+// Balance colors
+val YouOwe = Color(0xFFF44336) // Red
+val YouAreOwed = Color(0xFF4CAF50) // Green
+val Settled = Color(0xFF9E9E9E) // Grey
+
+// Expense category colors
+val CategoryFood = Color(0xFFFF9800) // Orange
+val CategoryDrinks = Color(0xFFD32F2F) // Red
+val CategoryGroceries = Color(0xFF689F38) // Green
+val CategoryTransportation = Color(0xFF1976D2) // Blue
+val CategoryAccommodation = Color(0xFF7B1FA2) // Purple
+val CategoryEntertainment = Color(0xFFC2185B) // Pink
+val CategoryShopping = Color(0xFF00796B) // Teal
+val CategoryUtilities = Color(0xFFFBC02D) // Yellow
+val CategoryHealth = Color(0xFF0097A7) // Cyan
+val CategoryGifts = Color(0xFFE64A19) // Deep Orange
+val CategoryTravel = Color(0xFF5D4037) // Brown
+val CategoryOther = Color(0xFF757575) // Grey
